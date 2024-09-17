211569f6b432e446e15b070b0ba33f67799c48b4
Игорь <wazuup.lw@mail.ru>
2024-09-17 10:14:42 +0300

    add code splitting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,62 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./homePage/HomePage";
-import Movies from "./movies/Movies";
-import Series from "./series/Series";
-import Cartoons from "./cartoons/Cartoons";
-import Favourites from "./favourites/Favourites";
-import Movie from "./movie/Movie";
-import Page404 from "./page404/Page404";
 import Layout from "./layout/Layout";
-import SearchResult from "./searchResult/SearchResult";
-import Login from "./login/Login";
-import Register from "./register/Register";
+
+const Movies = lazy(
+  () => import(/*webpackChunkName:"[Movies]"*/ "./movies/Movies")
+);
+const Series = lazy(
+  () => import(/*webpackChunkName:"[Series]"*/ "./series/Series")
+);
+const Cartoons = lazy(
+  () => import(/*webpackChunkName:"[Cartoons]"*/ "./cartoons/Cartoons")
+);
+const Favourites = lazy(
+  () => import(/*webpackChunkName:"[Favourites]"*/ "./favourites/Favourites")
+);
+const Movie = lazy(
+  () => import(/*webpackChunkName:"[Movie]"*/ "./movie/Movie")
+);
+const Page404 = lazy(
+  () => import(/*webpackChunkName:"[Page404]"*/ "./page404/Page404")
+);
+const Login = lazy(
+  () => import(/* webpackChunkName:"[Login]"*/ "./login/Login")
+);
+const Register = lazy(
+  () => import(/*webpackChunkName:"[Register]"*/ "./register/Register")
+);
+const SearchResult = lazy(
+  () =>
+    import(/*webpackChunkName: "[SearchResult]"*/ "./searchResult/SearchResult")
+);
 
 function Pages(): JSX.Element {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="series" element={<Series />} />
-        <Route path="cartoons" element={<Cartoons />} />
-        <Route path="favourites" element={<Favourites />} />
-        <Route path="search" element={<SearchResult />} />
-        <Route path="auth/login" element={<Login />} />
-        <Route path="auth/register" element={<Register />} />
-        <Route path=":movie/:id" element={<Movie />} />
-        <Route path="*" element={<Page404 />} />
-      </Route>
-    </Routes>
+    <Suspense
+      fallback={
+        <div className="h-screen flex items-center justify-center">
+          <p className="text-4xl">Загрузка...</p>
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="series" element={<Series />} />
+          <Route path="cartoons" element={<Cartoons />} />
+          <Route path="favourites" element={<Favourites />} />
+          <Route path="search" element={<SearchResult />} />
+          <Route path="auth/login" element={<Login />} />
+          <Route path="auth/register" element={<Register />} />
+          <Route path=":movie/:id" element={<Movie />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
