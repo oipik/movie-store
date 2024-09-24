3c50ab6d2455df86cc2f570832e74d81e1eb07c3
Игорь <wazuup.lw@mail.ru>
2024-09-24 10:26:47 +0300

    fix bug with favourite page

--- src/pages/favourites/Favourites.tsx
+++ src/pages/favourites/Favourites.tsx
@@ -4,6 +4,7 @@ import { Card, SwitcherTheme } from "../../components";
 
 const Favourites: React.FC = () => {
   const { favourites } = useAppSelector((state) => state.movies);
+  const isAuth = Boolean(useAppSelector((state) => state.auth.data));
 
   const movies = favourites?.map((movie, i) => {
     return (
@@ -18,11 +19,15 @@ const Favourites: React.FC = () => {
 
   return (
     <div className="flex flex-wrap justify-center gap-[40px] relative">
-      {movies?.length !== 0 ? (
-        movies
-      ) : (
+      {isAuth && movies?.length !== 0 && movies}
+      {isAuth && movies?.length === 0 && (
         <p className="text-4xl dark:text-white">
           Вы пока еще ничего не добавляли...
+        </p>
+      )}
+      {!isAuth && (
+        <p className="text-4xl dark:text-white">
+          Авторизуйтесь чтобы добавить в избранное
         </p>
       )}
       <div className="absolute bottom-48 left-[-130px]">
